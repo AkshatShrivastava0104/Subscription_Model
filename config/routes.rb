519a71a1651c333828491116ca630615bfb2a5b3
Akshat <121187184+AkshatShrivastava0104@users.noreply.github.com>
2025-05-07 19:15:44 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root "pages#home"
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
+end
---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Plan.create(name: "Free", price: 0)
+Plan.create(name: "Pro", price: 9.99)
